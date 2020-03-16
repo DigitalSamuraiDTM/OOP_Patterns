0513874207049a918df676ad5fc363e293db32e2
DigitalSamuraiDTM <andrey_nugaev@list.ru>
2020-03-16 21:58:47 +0500

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,9 @@
 package com.company;
 
-public class Main {
+//Шаблон Фабричный метод
 
+
+public class Main {
     public static void main(String[] args) {
 	System.out.println("FactoryMethod");
     }
